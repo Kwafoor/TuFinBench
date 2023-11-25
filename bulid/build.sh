84a99dbbe6ce3c026995d05539a1c69edeb02a1a
wangjunbo <junbo.w@outlook.com>
2023-11-25 11:07:34 +0800

    增加打包命令

--- bulid/build.sh
+++
@@ -1,5 +0,0 @@
-cd ../
-mvn package assembly:single
-cd ./build
-cp ../target/TuFinBench-1.0-SNAPSHOT-jar-with-dependencies.jar ./
-zip wang_cn.zip TuFinBench-1.0-SNAPSHOT-jar-with-dependencies.jar run.sh
